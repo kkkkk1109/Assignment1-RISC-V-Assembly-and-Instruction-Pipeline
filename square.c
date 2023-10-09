893b6835e084fd03afa08bc3b98e1847a69e784a
kkkkk1109 <134604810+kkkkk1109@users.noreply.github.com>
2023-10-09 15:22:50 +0800

    square.c

---
+++ square.c
@@ -0,0 +1,139 @@
+#include <stdint.h>
+#include <stdio.h>
+#define iteration 10
+int lz, lzc, i;
+float ans;
+void print_binary(uint64_t x) {
+    for (int i = 31; i >= 0; i--) {
+        putchar((x & (1ULL << i)) ? '1' : '0');
+    
+    }
+    printf("\n");
+}
+
+int count_leading_zeros(uint32_t x)
+{
+    x |= (x >> 1);
+    x |= (x >> 2);
+    x |= (x >> 4);
+    x |= (x >> 8);
+    x |= (x >> 16);
+    /* count ones (population count) */
+    x -= ((x >> 1) & 0x55555555);
+    x = ((x >> 2) & 0x33333333) + (x & 0x33333333);
+    x = ((x >> 4) + x) & 0x0f0f0f0f;
+    x += (x >> 8);       
+    x += (x >> 16);
+    
+    return (32 - (x & 0x7f));
+}
+float uint_to_float(uint32_t u){
+    //unsigned int exp=127+31-count_leading_zeros(u);
+    int c = count_leading_zeros(u);
+    unsigned int exp=127+31-c;
+    u <<= (c-8);
+    // printf("clzu%d\n",count_leading_zeros(u));
+    // int t=0;
+    // while ((u & (1 << 23)) == 0)
+    // { 
+    //     u <<= 1;
+    //     t++;
+    // }
+    uint32_t flt= (exp << 23) | (u & 0x7FFFFF);
+    return * (float *) &flt;
+}
+float division(float a,float b){
+    //change float into int 
+    int32_t ia = *(int32_t *)& a;
+    int32_t ib = *(int32_t *)& b;
+    //get the exponential
+    int32_t expa = (ia >> 23) & 0xff ;
+    int32_t expb = (ib >> 23) & 0xff;
+    // parameter used in rounded output
+    uint8_t  odd, rnd, sticky;
+    //get the significand
+    uint32_t siga= ((ia  & 0x7fffff) | 0x800000);
+    uint32_t sigb= ((ib  & 0x7fffff) | 0x800000);
+    //exponential output
+    int32_t expout= expa - expb +127;
+    //allign two numbers' significand
+    if(siga < sigb){
+        siga = siga << 1;
+        expout--;
+    }
+    // r means result
+    uint32_t r = 0;
+    // star division
+    for(int i= 0 ; i < 25 ; i++){
+        r= r << 1;
+        if(siga >= sigb){
+            siga = siga - sigb;
+            r = r | 1;
+        }
+        siga = siga << 1 ;
+    }
+    // sticky to see 
+    // sticky =(siga != 0);
+    // rnd =(r & 1);
+    // odd =(r & 2) != 0;
+    // r=(r>>1)+ (rnd & (sticky | odd));
+    r = r>>1;
+    int32_t sigout=r & 0x7fffff;
+    int32_t out= ((expout & 0xff) << 23) | (sigout);
+    return *(float *) &out;
+}
+float addition(float a,float b){
+    //change float into int
+    int32_t ia = *(int32_t *)& a;
+    int32_t ib = *(int32_t *)& b;
+    //get the exponential
+    int32_t expa = (ia >> 23) & 0xff ;
+    int32_t expb = (ib >> 23) & 0xff;
+    //get the significand
+    uint32_t siga= ((ia & 0x7fffff) | 0x800000);
+    uint32_t sigb= ((ib & 0x7fffff) | 0x800000);
+    //exponential output
+    int32_t expout=0;
+    int dif= expa - expb;
+    if(dif >= 0){
+        sigb = sigb >> dif;
+        expout = expa;
+    }
+    else{
+        dif = -dif;
+        siga = siga >> dif;
+        expout = expb;
+    }
+    uint32_t sigout = siga + sigb;
+    if(sigout >> 24 == 1){
+        sigout = sigout >>1;
+        expout = expout + 1;
+    }
+    int32_t out=(0 & 1 << 31) | ((expout & 0xff) << 23) | ((sigout) & 0x7fffff);
+    return *(float *) &out;
+}
+int main(){
+    uint32_t r=500;
+    lz =count_leading_zeros(r);
+    lzc = (32 - lz) / 2;
+    ans=uint_to_float(r >> lzc);
+    float t,c;
+    for (i = 0 ; i < iteration ; i++){
+        printf("---------------%d-----------------\n",i);
+        t = division(r,ans);
+        printf("t1= %f\n",t);
+        t = addition(t,ans);
+        printf("t2= %f\n",t);
+        t = division(t,2);
+        printf("t3= %f\n",t);
+        ans=t;
+        if(ans == c){
+            printf("The iteration time is: %d\n",i+1);
+            break;
+        }
+        c = ans;
+        printf("xn=%f\n",t);
+    }
+
+    return 0; 
+}
